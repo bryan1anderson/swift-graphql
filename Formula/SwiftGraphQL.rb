e890ff4abae6d6b2fafd2be6b61accc117cbbd38
Bryan Anderson <bryan1anderson@gmail.com>
2025-05-20 11:09:21 -0600

    Update SwiftGraphQL.rb

--- Formula/SwiftGraphQL.rb
+++ Formula/SwiftGraphQL.rb
@@ -2,12 +2,11 @@ class Swiftgraphql < Formula
   desc "Code generator for SwiftGraphQL library"
   homepage "https://swift-graphql.org"
   license "MIT"
+  version "6.0.1"
   
-  url "https://github.com/maticzav/swift-graphql/archive/5.1.2.tar.gz"
-  sha256 "a3b7af209356dc1362b807fd9a4f33dde47c2b815e034e6c7016f19968e8d33e"
+  url "https://github.com/bryan1anderson/swift-graphql/archive/refs/tags/6.0.1.tar.gz"
+  sha256 "2510d795cc04862148abd99f4962cabf361019332a4254590ee43da8cf3e1611"
   
-  head "https://github.com/maticzav/swift-graphql.git"
-
   depends_on :xcode
   uses_from_macos "libxml2"
   uses_from_macos "swift"
@@ -16,7 +15,7 @@ class Swiftgraphql < Formula
     system "make", "install", "PREFIX=#{prefix}"
   end
 
-  def test
-    system "true"
+  test do
+    system "#{bin}/swift-graphql", "--version"
   end
 end
